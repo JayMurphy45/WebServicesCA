a6cb5b96e725b280b29fa606d4bd807d54cceca4
JayMurphy45 <B00143682@mytudublin.ie>
2025-03-27 11:57:45 +0000

    Update test.py

--- test.py
+++ test.py
@@ -12,13 +12,35 @@ class TestFastAPIApp(unittest.TestCase):
         self.assertEqual(response.json(), {"Hello": "World"})
 
     def test_get_single_product(self):
-        response = self.client.get("/getSingleProduct/test_id")
+        response = self.client.get("/getSingleProduct/AUTO008")
         self.assertEqual(response.status_code, 200)
         
 
     def test_get_all_products(self):
         response = self.client.get("/getAll")
         self.assertEqual(response.status_code, 200)
+
+    def test_add_new(self):
+        response = self.client.post("/addNew/AUTO008/Motor/1.0/1/test")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_delete_one(self):
+        response = self.client.delete("/deleteOne/AUTO008")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_starts_with(self):
+        response = self.client.get("/startsWith/m")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_paginate(self):
+        response = self.client.get("/paginate/1/2")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_convert(self):
+        response = self.client.get("/convert/AUTO008")
+        self.assertEqual(response.status_code, 200)
+    
+
         
 if __name__ == "__main__":
     unittest.main()
